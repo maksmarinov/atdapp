6e8b30581d5a53afd7955527c34b57451dbc265b
Maksim Marinov <maks1412@gmail.com>
2025-03-31 22:19:33 +0300

    profile dynamic

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,8 @@ import { getCurrentUser } from "../lib/utils";
 import SlidingMenu from "../components/SlidingMenu";
 import { User } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProfilePage() {
   const user: User = await getCurrentUser();
   const username: string = user.username;
